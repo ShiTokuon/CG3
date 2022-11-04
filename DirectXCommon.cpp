a02e26c65d918b66db3339c921370c377ddd3b6e
ShiTokuon <k021g1160@g.neec.ac.jp>
2022-11-04 10:37:36 +0900

    02_03点からジオメトリを生成

--- DirectXCommon.cpp
+++ DirectXCommon.cpp
@@ -156,6 +156,7 @@ void DirectXCommon::ClearRenderTarget() {
 
 	// 全画面クリア        Red   Green Blue  Alpha
 	float clearColor[] = {0.1f, 0.25f, 0.5f, 0.0f}; // 青っぽい色
+	/*float clearColor[] = { 0.0f, 0.0f, 0.0f, 0.0f };*/
 	commandList_->ClearRenderTargetView(rtvH, clearColor, 0, nullptr);
 }
 
